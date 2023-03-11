6340b1697130e7fca1d59c284f31167e12a90749
Henrique Chaves <canaldomonza@gmail.com>
2023-03-11 15:56:21 -0300

    feat: add function transfer payment

--- internal/usecases/usecases.go
+++ internal/usecases/usecases.go
@@ -1,11 +1,14 @@
 package usecases
 
+import "errors"
+
 type User struct {
 	ID      string
 	Balance int64
 }
 type IUserRepository interface {
 	FindUserByID(id string) (*User, error)
+	UpdateBalance(id string, amount int64) error
 }
 type UserUsecase struct {
 	repo IUserRepository
@@ -23,4 +26,28 @@ func (uc *UserUsecase) GetUser(id string) (*User, error) {
 		return nil, err
 	}
 	return user, nil
+}
+
+func (uc *UserUsecase) TransferPayment(payer, receive string, amount int64) error {
+	userPayer, err := uc.repo.FindUserByID(payer)
+	if err != nil {
+		return err
+	}
+	if userPayer.Balance < amount {
+		return errors.New("insufficient funds")
+	}
+
+	userReceive, err := uc.repo.FindUserByID(receive)
+	if err != nil {
+		return err
+	}
+	err = uc.repo.UpdateBalance(userPayer.ID, userPayer.Balance-amount)
+	if err != nil {
+		return err
+	}
+	err = uc.repo.UpdateBalance(userReceive.ID, userReceive.Balance+amount)
+	if err != nil {
+		return err
+	}
+	return nil
 }
