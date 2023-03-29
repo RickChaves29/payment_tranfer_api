bbbf1f22b61ceae7961a579a09a71e974c95a74c
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-03-29 19:26:21 -0300

    feat: add create user and find user by email on user layer

--- internal/domain/user/usecase.go
+++ internal/domain/user/usecase.go
@@ -9,3 +9,19 @@ func NewUserUsecase(ur IUserRepository) *UserUsecase {
 		userRepo: ur,
 	}
 }
+
+func (uc *UserUsecase) CreateNewUser(data CreateUserEntity) error {
+	err := uc.userRepo.Save(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (uc *UserUsecase) GetUserByEmail(email string) (*UserEntity, error) {
+	user, err := uc.userRepo.FindUserByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
