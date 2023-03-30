48e8800df568b7f955e47d39b6bfa90f36aa5294
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-03-30 01:47:24 -0300

    feat: add transfer payment method

--- internal/domain/user/usecase.go
+++ internal/domain/user/usecase.go
@@ -1,5 +1,9 @@
 package user
 
+import (
+	"errors"
+)
+
 type UserUsecase struct {
 	userRepo IUserRepository
 }
@@ -24,4 +28,27 @@ func (uc *UserUsecase) GetUserByEmail(email string) (*UserEntity, error) {
 		return nil, err
 	}
 	return user, nil
+}
+
+func (uc *UserUsecase) TransferPayment(data UserTransferPaymentEntity) error {
+	payer, err := uc.userRepo.FindUserByEmail(data.Payer)
+	if err != nil {
+		return err
+	}
+	receive, err := uc.userRepo.FindUserByEmail(data.Receive)
+	if err != nil {
+		return err
+	}
+	if payer.Balance < data.Amount {
+		return errors.New("insufficient founds")
+	}
+	err = uc.userRepo.UpdateBalance(payer.ID, payer.Balance-data.Amount)
+	if err != nil {
+		return err
+	}
+	err = uc.userRepo.UpdateBalance(receive.ID, receive.Balance+data.Amount)
+	if err != nil {
+		return err
+	}
+	return nil
 }
