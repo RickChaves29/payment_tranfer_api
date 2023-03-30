f5bf7c7b2f6e9ae35738524dcf63a6581b8d5416
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-03-30 00:19:20 -0300

    test: add tests from find all transfers method

--- internal/domain/transfer/usecase_test.go
+++ internal/domain/transfer/usecase_test.go
@@ -28,6 +28,10 @@ func (rt *TransferRepoTest) Save(userID uint64, data transfer.CreateTransfer) er
 	return nil
 }
 
+func (rt *TransferRepoTest) FindAllTransfers(userId uint64) ([]transfer.TransferEntity, error) {
+	return rt.repo, nil
+}
+
 func TestIfCreateNewTransferIsSuccess(t *testing.T) {
 	rt := NewRepoTest([]transfer.TransferEntity{})
 	uc := transfer.NewTaskferUsecase(rt)
@@ -41,6 +45,43 @@ func TestIfCreateNewTransferIsSuccess(t *testing.T) {
 	t.Run("should create new transfer success", func(t *testing.T) {
 		if len(rt.repo) == 0 {
 			t.Errorf("have %v want %v", len(rt.repo), 1)
+		}
+	})
+}
+
+func TestIfListAllTransfersIsSuccess(t *testing.T) {
+	transfers := []transfer.TransferEntity{
+		{
+			ID:           1,
+			PayerName:    "any 1",
+			ReceiveName:  "any 2",
+			Amount:       100,
+			TransferDate: time.Now().GoString(),
+		},
+		{
+			ID:           2,
+			PayerName:    "any 1",
+			ReceiveName:  "any 3",
+			Amount:       200,
+			TransferDate: time.Now().GoString(),
+		},
+		{
+			ID:           3,
+			PayerName:    "any 1",
+			ReceiveName:  "any 5",
+			Amount:       600,
+			TransferDate: time.Now().GoString(),
+		},
+	}
+
+	rt := NewRepoTest(transfers)
+	uc := transfer.NewTaskferUsecase(rt)
+
+	t.Run("should return list of transfers when user request", func(t *testing.T) {
+		transfers, _ := uc.ListAllTransfers(2)
+
+		if len(rt.repo) != len(transfers) {
+			t.Errorf("have %v want %v", len(rt.repo), len(transfers))
 		}
 	})
 }
