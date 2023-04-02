3f1cc76741fb2d6e605514530d9995076c479962
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-04-01 22:08:12 -0300

    feat: add method find user by id on interface

--- internal/domain/user/interface.go
+++ internal/domain/user/interface.go
@@ -3,5 +3,6 @@ package user
 type IUserRepository interface {
 	Save(data CreateUserEntity) error
 	FindUserByEmail(email string) (*UserEntity, error)
+	FindUserById(id uint64) (*UserEntity, error)
 	UpdateBalance(id uint64, amount uint64) error
 }
